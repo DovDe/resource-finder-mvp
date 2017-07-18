8123682298442bb02bd04d11b4b6de15c4a9287b
Dov DeKeyser <dovdekeyser@yahoo.com>
2017-07-18 08:56:24 -0700

    added gulp-gh-pages

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resourceFinderMvpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the resourceFinderMvpApp
+ */
+angular.module('resourceFinderMvpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resourceFinderMvpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the resourceFinderMvpApp
+ */
+angular.module('resourceFinderMvpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
