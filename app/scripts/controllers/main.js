f50c7f490c2e823d9d24e918c468ba8dbc379c10
Dov DeKeyser <dovdekeyser@yahoo.com>
2017-07-19 10:32:39 -0700

    added map functionality with autocomplete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,34 @@
  * # MainCtrl
  * Controller of the resourceFinderMvpApp
  */
+
+
 angular.module('resourceFinderMvpApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+.controller('MainCtrl', function(NgMap) {
+var vm = this;
+// places changed function
+vm.placeChanged = function() {
+  vm.place = this.getPlace();
+  // console.log('location', vm.place.geometry.location);
+  vm.map.setCenter(vm.place.geometry.location);
+
+};  // close placeChanged
+
+//  get map function
+  NgMap.getMap().then(function(map) {
+   vm.map = map;
   });
+
+  vm.centerChanged = function(event) {
+    vm.pauseLoading=true;
+    $timeout(function() {
+
+      vm.pauseLoading=false;
+
+      vm.home = vm.map.getCenter();
+    
+    }, 1000);
+      };
+
+
+});  //close controller
